7573afa02d85d17f912ccbfcb20c91a510907bc2
min-gukang <route2054@gmail.com>
2020-12-24 08:50:40 +0900

    lobby

--- routes/api/lobby/filter.js
+++ routes/api/lobby/filter.js
@@ -1,3 +1,28 @@
-module.exports = (req, res) => {
-  res.json('filter');
-}
+const { verify } = require('jsonwebtoken');
+const { Post } = require('../../../models');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = async (req, res) => {
+  let token = req.cookies.x_auth;
+
+  if (!token) {
+    return res.json({ code: 400, message: 'not token' });
+  } else {
+    try {
+      verify(token, process.env.SECRET);
+      let userData = await Post.findAll({
+        where: {
+          acreage: req.query.acreage,
+          housingType: req.query.housingType,
+          space: req.query.space,
+        },
+      });
+      userData = userData.map((el) => el.dataValues);
+      res.status(200).send(userData);
+    } catch (err) {
+      res.json({ code: 400, postLoad: false });
+    }
+  }
+};
--- routes/api/lobby/likepost.js
+++ routes/api/lobby/likepost.js
@@ -1,3 +1,24 @@
-module.exports = (req, res) => {
-  res.json('likepost');
-}
+const { verify } = require('jsonwebtoken');
+const { Post } = require('../../../models');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = async (req, res) => {
+  let token = req.cookies.x_auth;
+
+  if (!token) {
+    return res.json({ code: 400, message: 'not token' });
+  } else {
+    try {
+      let isVerify = verify(token, process.env.SECRET);
+      await Post.increment(
+        { like: 1 },
+        { where: { userId: isVerify.oAuthId } }
+      );
+      res.json({ code: 200, updateSuccess: true });
+    } catch (err) {
+      res.json({ code: 500, updateSuccess: false });
+    }
+  }
+};
