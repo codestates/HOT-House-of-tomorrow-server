afe3b4b6b178b455d4d99dec6304a118933668f9
HyeYeonCha <gpdus335@naver.com>
2020-12-23 23:19:54 +0900

    refactor/Complete refactoring using Prettier

--- routes/api/auth/deleteuser.js
+++ routes/api/auth/deleteuser.js
@@ -1,19 +1,18 @@
-const { User } = require('../../../models'); 
+const { User } = require('../../../models');
 const jwt = require('jsonwebtoken');
-const config = require('../../../config/index')
+const config = require('../../../config/index');
 const { SECRET } = config;
 
 module.exports = async (req, res) => {
   let token = req.cookies.x_auth;
 
-  if(!token){
-    res.status(400).json({message:"not token"});
-  }
-  else{
+  if (!token) {
+    res.status(400).json({ message: 'not token' });
+  } else {
     let tokenData = jwt.verify(token, SECRET);
     await User.destroy({
-      where: {email: tokenData.email}
+      where: { email: tokenData.email },
     });
-    res.json({message: "success"});
-  } 
+    res.json({ message: 'success' });
+  }
 };
