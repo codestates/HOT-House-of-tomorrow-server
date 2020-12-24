0a5505b3a6df9b51811072a644a2c988fe812afa
min-gukang <route2054@gmail.com>
2020-12-24 18:09:58 +0900

    feature/post: add read,write,update,delete

--- routes/api/post/delete.js
+++ routes/api/post/delete.js
@@ -1,3 +1,26 @@
-module.exports = (req, res) => {
-  res.json('delete');
-}
+const { Post, Comment } = require('../../../models');
+const jwt = require('jsonwebtoken');
+const config = require('../../../config/index');
+const { SECRET } = config;
+
+module.exports = async (req, res) => {
+  let token = req.cookies.x_auth;
+  const { userId, postId } = req.body;
+
+  if (!token) {
+    res.status(400).json({ message: 'not token' });
+  } else {
+    try {
+      jwt.verify(token, SECRET);
+      await Comment.destroy({
+        where: { postId },
+      });
+      await Post.destroy({
+        where: { userId },
+      });
+      res.status(200).json({ postDeleted: true });
+    } catch (err) {
+      res.status(500).json({ postDeleted: false });
+    }
+  }
+};
