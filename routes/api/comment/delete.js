baf5b75005551c5ef323633805be166f4a6d974e
HyeYeonCha <gpdus335@naver.com>
2020-12-26 20:35:53 +0900

    feature/comment: add comment api

--- routes/api/comment/delete.js
+++ routes/api/comment/delete.js
@@ -1,3 +1,29 @@
-module.exports = (req, res) => {
-  res.json('delete');
-}
+const { User, Comment } = require('../../../models');
+const jwt = require('jsonwebtoken');
+const config = require('../../../config/index');
+const { SECRET } = config;
+
+module.exports = async (req, res) => {
+  let token = req.cookies.x_auth;
+  const { postId, commentId } = req.body;
+
+  let tokenData = jwt.verify(token, SECRET);
+  let userInfo = await User.findOne({
+    attributes: ['nickname'],
+    where: { email: tokenData.email },
+  });
+
+  if (!userInfo) res.status(500).json({ message: 'no nickname' });
+  else {
+    await Comment.destroy({
+      where: {
+        userId: userInfo.nickname,
+        postId: postId,
+        id: commentId,
+      },
+    });
+    res.json({
+      deleteComment: true,
+    });
+  }
+};
