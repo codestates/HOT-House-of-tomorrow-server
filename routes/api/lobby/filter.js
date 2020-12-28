d996c78b3ddb453f8ff2ae2bdedb29441f71c2df
min-gukang <route2054@gmail.com>
2020-12-28 15:15:41 +0900

    fix/lobby: edit filter

--- routes/api/lobby/filter.js
+++ routes/api/lobby/filter.js
@@ -1,5 +1,5 @@
 const { verify } = require('jsonwebtoken');
-const { Post, User } = require('../../../models');
+const { Post, User, Comment } = require('../../../models');
 const config = require('../../../config/index');
 const { SECRET } = config;
 
@@ -11,31 +11,46 @@ module.exports = async (req, res) => {
   } else {
     try {
       verify(token, SECRET);
-      let obj ={};
+      let obj = {};
       let acreage = req.query.acreage;
       let housingType = req.query.housingType;
       let space = req.query.space;
+      let color = req.query.color;
 
-      if(acreage && !housingType && !space) {
-        obj={acreage};
-      } else if(!acreage && housingType && !space) {
-        obj={housingType};
-      } else if(!acreage && !housingType && space) {
-        obj={space};
-      } else if(acreage && housingType && !space) {
-        obj = {acreage, housingType};
-      } else if(acreage && !housingType && space) {
-        obj = {acreage, space};
-      } else if(!acreage && housingType && space) {
-        obj = {housingType, space};
-      } else if(acreage && housingType && space) {
-        obj = {acreage, housingType, space};
+      if (acreage && !housingType && !space && !color) {
+        obj = { acreage };
+      } else if (!acreage && housingType && !space && !color) {
+        obj = { housingType };
+      } else if (!acreage && !housingType && space && !color) {
+        obj = { space };
+      } else if (!acreage && !housingType && !space && color) {
+        obj = { color };
+      } else if (acreage && housingType && !space && !color) {
+        obj = { acreage, housingType };
+      } else if (acreage && !housingType && space && !color) {
+        obj = { acreage, space };
+      } else if (acreage && !housingType && !space && color) {
+        obj = { acreage, color };
+      } else if (!acreage && housingType && space && !color) {
+        obj = { housingType, space };
+      } else if (!acreage && housingType && !space && color) {
+        obj = { housingType, color };
+      } else if (!acreage && !housingType && space && color) {
+        obj = { space, color };
+      } else if (acreage && housingType && space && !color) {
+        obj = { acreage, housingType, space };
+      } else if (acreage && housingType && !space && color) {
+        obj = { acreage, housingType, color };
+      } else if (!acreage && housingType && space && color) {
+        obj = { housingType, space, color };
+      } else if (acreage && housingType && space && color) {
+        obj = { acreage, housingType, space, color };
       }
 
       let postData = await Post.findAll({
         include: {
           model: User,
-          attributes: ['nickname', 'profileImg'],
+          attributes: ['nickname', 'profileImg', 'introduction'],
         },
         attributes: {
           exclude: [
@@ -44,15 +59,35 @@ module.exports = async (req, res) => {
             'acreage',
             'housingType',
             'space',
-            'description',
           ],
         },
         where: {
-          ...obj
+          ...obj,
         },
       });
-      postData = postData.map((el) => el.dataValues);
-      res.status(200).json({results : postData});
+      let commentData = await Comment.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['nickname', 'profileImg'],
+          },
+        ],
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'id', 'userId', 'date'],
+        },
+      });
+
+      postData = postData.map((el) => {
+        let data = el.dataValues;
+        let getComment = commentData
+          .filter((el2) => el2.dataValues.postId === data.id)
+          .map((el3) => el3.dataValues);
+
+        data['comments'] = getComment;
+        return data;
+      });
+
+      res.status(200).json({ results: postData });
     } catch (err) {
       res.status(400).json({ postLoad: false });
     }
