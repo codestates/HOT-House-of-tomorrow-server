54629bfe28e6a9a5c1c4ab558462a36bf1a58bea
HyeYeonCha <gpdus335@naver.com>
2020-12-28 11:52:58 +0900

    fix/getpost: add introduction column

--- routes/api/lobby/getposts.js
+++ routes/api/lobby/getposts.js
@@ -15,30 +15,43 @@ module.exports = async (req, res) => {
         include: [
           {
             model: User,
-            attributes: ['nickname', 'profileImg']
+            attributes: ['nickname', 'profileImg', 'introduction'],
           },
         ],
-        attributes : {exclude : ['createdAt', 'updatedAt', 'acreage', 'housingType', 'space', 'userId']}
-      })
+        attributes: {
+          exclude: [
+            'createdAt',
+            'updatedAt',
+            'acreage',
+            'housingType',
+            'space',
+            'userId',
+          ],
+        },
+      });
       let commentData = await Comment.findAll({
-        include :[
+        include: [
           {
             model: User,
-            attributes: ['nickname', 'profileImg']
+            attributes: ['nickname', 'profileImg'],
           },
         ],
-        attributes : {exclude : ['createdAt', 'updatedAt', 'id', 'userId', 'date']}
-      })
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'id', 'userId', 'date'],
+        },
+      });
 
-        postData = postData.map((el) => {
-          let data = el.dataValues;
-          let getComment = commentData.filter((el2) => el2.dataValues.postId === data.id).map((el3) => el3.dataValues);
-          
-          data["comments"] = getComment
-          return data
-        });
-      
-      res.status(200).json({results : postData});
+      postData = postData.map((el) => {
+        let data = el.dataValues;
+        let getComment = commentData
+          .filter((el2) => el2.dataValues.postId === data.id)
+          .map((el3) => el3.dataValues);
+
+        data['comments'] = getComment;
+        return data;
+      });
+
+      res.status(200).json({ results: postData });
     } catch (err) {
       res.status(404).json({ postLoad: false });
     }
