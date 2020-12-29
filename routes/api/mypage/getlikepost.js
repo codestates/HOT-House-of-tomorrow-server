144e6d45439d5d259126c0c56255a1ab742091b8
HyeYeonCha <gpdus335@naver.com>
2020-12-29 11:31:48 +0900

    fix/mypage: Modify to fit the document

--- routes/api/mypage/getlikepost.js
+++ routes/api/mypage/getlikepost.js
@@ -7,27 +7,31 @@ module.exports = async (req, res) => {
   let token = req.cookies.x_auth;
   if (!token) res.json({ message: 'no token' });
   else {
-    let tokenData = jwt.verify(token, SECRET);
-    let userInfo = await User.findOne({
-      where: { email: tokenData.email },
-    });
-    let likes = await User.findOne({
-      attributes: ['likePosts'],
-      where: { email: userInfo.email },
-    });
-    let { likePosts } = likes;
-    if (likePosts === '0') res.json({ message: 'no like post' });
-    else {
-      likePosts = likePosts.split(',');
-      let results = [];
-      for (let postId of likePosts) {
-        let result = await Post.findOne({
-          where: { id: postId },
-        });
-        results.push(result);
-      }
+    try {
+      let tokenData = jwt.verify(token, SECRET);
+      let userInfo = await User.findOne({
+        where: { email: tokenData.email },
+      });
+      let likes = await User.findOne({
+        attributes: ['likePosts'],
+        where: { email: userInfo.email },
+      });
+      let { likePosts } = likes;
+      if (likePosts === '0') res.json({ message: 'no like post' });
+      else {
+        likePosts = likePosts.split(',');
+        let results = [];
+        for (let postId of likePosts) {
+          let result = await Post.findOne({
+            where: { id: postId },
+          });
+          results.push(result);
+        }
 
-      res.json({ results: results });
+        res.json({ getlikepost: true, post: results });
+      }
+    } catch (err) {
+      res.status(500).json({ getlikepost: false, error: err });
     }
   }
 };
--- routes/api/mypage/getpost.js
+++ routes/api/mypage/getpost.js
@@ -7,21 +7,25 @@ module.exports = async (req, res) => {
   let token = req.cookies.x_auth;
   if (!token) res.json({ message: 'no token' });
   else {
-    let tokenData = jwt.verify(token, SECRET);
-    let userInfo = await User.findOne({
-      where: { email: tokenData.email },
-    });
+    try {
+      let tokenData = jwt.verify(token, SECRET);
+      let userInfo = await User.findOne({
+        where: { email: tokenData.email },
+      });
 
-    let userPosts = await Post.findAll({
-      include: {
-        model: User,
-        attribute: ['nickname'],
-      },
-      where: { userId: userInfo.nickname },
-    });
+      let userPosts = await Post.findAll({
+        include: {
+          model: User,
+          attribute: ['nickname'],
+        },
+        where: { userId: userInfo.nickname },
+      });
 
-    !userPosts
-      ? res.json({ message: 'no post' })
-      : res.json({ userPosts: userPosts });
+      !userPosts
+        ? res.json({ getMyPost: true, message: 'no post' })
+        : res.json({ getMyPost: true, userPosts: userPosts });
+    } catch (err) {
+      res.status(500).json({ getMyPost: false, error: err });
+    }
   }
 };
