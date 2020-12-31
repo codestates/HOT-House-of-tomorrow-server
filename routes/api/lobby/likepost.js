b7bf87b8ad17e42889837ffa18722cf5e0388cad
HyeYeonCha <gpdus335@naver.com>
2020-12-31 20:37:20 +0900

    fix/lobby: Add decrease like in likepost, add isAuth in getposts

--- routes/api/lobby/likepost.js
+++ routes/api/lobby/likepost.js
@@ -13,19 +13,23 @@ module.exports = async (req, res) => {
     try {
       let isVerify = verify(token, SECRET);
 
-      await Post.increment({ like: 1 }, { where: { id: postId } });
-
       let prevLikePostValue = await User.findOne({
         where: { email: isVerify.email },
       }).then((data) => data.dataValues.likePosts);
 
-      if (prevLikePostValue.indexOf(postId) !== -1) {
+      let preLikePosts = prevLikePostValue
+        .split(',')
+        .map((item) => parseInt(item));
+
+      if (preLikePosts.includes(postId)) {
         // 좋아요 취소
-        let preLikePosts = prevLikePostValue.split(',');
         let results = '0';
         for (let el of preLikePosts) {
-          if (el != postId && el !== '0') results = results + ',' + el;
+          if (el !== postId && el !== 0) {
+            results = results + ',' + el;
+          }
         }
+        await Post.decrement(['like'], { where: { id: postId } });
         await User.update(
           {
             likePosts: results,
@@ -38,9 +42,10 @@ module.exports = async (req, res) => {
         let userInfo = await User.findOne({
           where: { email: isVerify.email },
         });
-        res.status(200).json({ updateSuccess: true, userInfo: userInfo });
+        res.status(200).json({ dislikeSuccess: true, userInfo: userInfo });
       } else {
         // 좋아요
+        await Post.increment({ like: 1 }, { where: { id: postId } });
         await User.update(
           {
             likePosts: prevLikePostValue + ',' + postId,
