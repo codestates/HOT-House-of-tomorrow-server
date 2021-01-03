e1fcef1c13fe1f38806fd7e6a4556b95e3604a17
min-gukang <route2054@gmail.com>
2021-01-03 20:22:29 +0900

    fix/index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const uploadImg = require('./routes/api/utils/uploadimg');
 // Middle-ware
 app.use(
   cors({
-    origin: ' http://house-of-tomorrow.s3-website.ap-northeast-2.amazonaws.com/',
+    origin: 'http://house-of-tomorrow.s3-website.ap-northeast-2.amazonaws.com/',
     credentials: true,
     exposeHeaders: 'last-page',
   }),
