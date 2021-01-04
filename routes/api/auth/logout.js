42e4631e91695990fc00a9a4e3bd8d88c1451352
min-gukang <route2054@gmail.com>
2021-01-04 13:44:15 +0900

    fix/auth: delete logout.js

--- routes/api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  let token = req.cookies.x_auth;
-  if(token)
-    res.clearCookie('x_auth').json({ logoutSuccess: true });
-  else
-    res.status(500).json({logoutSuccess: false});
-};
