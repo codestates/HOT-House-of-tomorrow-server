3f1482f794c206e9d49447d1b41a8a5bca659b3f
min-gukang <route2054@gmail.com>
2021-01-04 13:47:07 +0900

    fix/auth: edit index.js

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,6 +1,5 @@
 const isAuth = require('./isAuth');
 const login = require('./login');
-const logout = require('./logout');
 const getUser = require('./getuser');
 const updateUser = require('./updateuser');
 const deleteUser = require('./deleteuser');
@@ -10,7 +9,6 @@ const router = express.Router();
 
 router.get('/isAuth', isAuth);
 router.post('/login', login);
-router.get('/logout', logout);
 router.get('/getuser', getUser);
 router.post('/updateuser', updateUser);
 router.get('/deleteuser', deleteUser);
